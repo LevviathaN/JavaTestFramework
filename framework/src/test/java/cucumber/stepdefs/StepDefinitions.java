d1c28892b0d1f4f6e1ed37f593695a2107a99470
Yurii Zosin <yurii.zosin@fortegrp.com>
2021-12-03 15:00:41 +0200

    rolled back changes for scrollTo StepDef updated token for SF

--- framework/src/test/java/cucumber/stepdefs/StepDefinitions.java
+++ framework/src/test/java/cucumber/stepdefs/StepDefinitions.java
@@ -398,13 +398,15 @@ public class StepDefinitions extends SeleniumHelper {
      *
      * @author Andrii Yakymchuk
      */
-    //todo: make this stepdef with 2 parameters and update all scenarios that uses it
+    //todo: this step doesn't work with StepdefBuilder logic
     @And("^I should scroll to the \"(top|bottom)\" of the page$")
     public void i_should_scroll_to_top_bottom_of_the_page(String value) {
-        StepDefinitionBuilder stepDef = new StepDefinitionBuilder();
-        stepDef.setAction(ActionsWithTwoParameters.SCROLL, "0", value)
-                .setReporterLog("Executing step: I should scroll to the " + value + " of the page")
-                .execute();
+        Reporter.log("Executing step: I should scroll to the " + value + " of the page");
+        if (value.equals("top")) {
+            scrollToTopOfPage();
+        } else if (value.equals("bottom")) {
+            scrollToBottomOfPage();
+        }
     }
 
     @And("I select \"([^\"]*)\" from \"([^\"]*)\" element")
